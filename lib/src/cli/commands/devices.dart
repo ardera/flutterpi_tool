aa517584462351f64b85c5d73c282d0e4eb67f6d
Hannes Winkler <hanneswinkler2000@web.de>
2025-08-23 20:45:08 +0200

    fix warnings

--- lib/src/cli/commands/devices.dart
+++ lib/src/cli/commands/devices.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:args/command_runner.dart';
 import 'package:flutterpi_tool/src/cli/command_runner.dart';
-import 'package:flutterpi_tool/src/cli/flutterpi_command.dart';
 import 'package:flutterpi_tool/src/fltool/common.dart';
 import 'package:flutterpi_tool/src/fltool/globals.dart' as globals;
 import 'package:flutterpi_tool/src/config.dart';
