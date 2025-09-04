1ce9a89ca25e8857bca16d12aca5350b754df4fb
Hannes Winkler <hanneswinkler2000@web.de>
2025-09-04 09:41:09 +0200

    fix device id specification
    needs to happen before verifyThenRunCommand, since that's where the flutter devices are queried.

--- lib/src/cli/commands/test.dart
+++ lib/src/cli/commands/test.dart
@@ -1,20 +1,6 @@
 import 'package:flutterpi_tool/src/cli/flutterpi_command.dart';
 import 'package:flutterpi_tool/src/fltool/common.dart' as fl;
-import 'package:flutterpi_tool/src/fltool/globals.dart' as globals;
 
 class TestCommand extends fl.TestCommand with FlutterpiCommandMixin {
   TestCommand();
-
-  @override
-  Future<fl.FlutterCommandResult> runCommand() {
-    final specifiedDeviceId = stringArg(
-      fl.FlutterGlobalOptions.kDeviceIdOption,
-      global: true,
-    );
-    if (specifiedDeviceId != null) {
-      globals.deviceManager?.specifiedDeviceId = specifiedDeviceId;
-    }
-
-    return super.runCommand();
-  }
 }
